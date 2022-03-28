7577fa17088d4c99f6cfe777491c5088f0540d32
asep wahyudin <awhyyoey@gmail.com>
2022-03-28 16:32:35 +0700

    commit

--- backend/src/config.js
+++ backend/src/config.js
@@ -57,10 +57,10 @@ const MINT_PRICE = 0.015; // Minting price per NFT. Rinkeby = ETH, Polygon = MAT
 const TOKENS_PER_MINT = 1 // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PUBLIC_MINT_START_DATE = "2022-03-20T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
+const PUBLIC_MINT_START_DATE = "2022-04-20T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = null; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = "2022-03-25T11:30:48+00:00";; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0x873c7A7889b25F339B3e015C3d0F81CE8D9c8CB4"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
